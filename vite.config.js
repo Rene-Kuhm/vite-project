6eb670fff9782d0ec897bdae9819e8ab0b527ceb
Rene Kuhm <renekuhm2@gmail.com>
2024-03-24 13:21:59 -0300

    se arreglo vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,19 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { createHtmlPlugin } from 'vite-plugin-html';
 
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins:[
+  plugins: [
     react(),
+    createHtmlPlugin({
+      inject: {
+        data: {
+          title: 'Tecnodespegue', // Aquí va el título que quieres para tu página
+          injectScript: `<script src="..."></script>`, // Si necesitas inyectar algún script
+          // otros datos que desees inyectar
+        },
+      },
+      minify: true,
+    }),
   ],
-})
+});
